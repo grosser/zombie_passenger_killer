b3e53ea62c6f5c27a039710955adcdb9bb385457
grosser <grosser.michael@gmail.com>
2011-09-08 14:21:45 +0200

    specs

--- lib/zombie_passenger_killer.rb
+++ lib/zombie_passenger_killer.rb
@@ -1,3 +1,67 @@
 class ZombiePassengerKiller
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
+
+  def initialize(options)
+    @CPU_HISTORY = {}
+    @MAX_HISTORY = options[:history] || 5
+    @MAX_FAILS = options[:max]
+    @MAX_CPU = options[:cpu] || 70
+    @GRACE_TIME = options[:grace] || 5
+    @INTERVAL = options[:interval] || 5
+    @PASSENGER_PROCESS_PATTERN = options[:pattern] || ' Rack: '
+  end
+
+  def store_current_cpu(processes)
+    keys_to_remove = @CPU_HISTORY.keys - processes.map{|x| x[:pid] }
+    keys_to_remove.each{|k| !@CPU_HISTORY.delete k }
+
+    processes.each do |process|
+      @CPU_HISTORY[process[:pid]] ||= []
+      @CPU_HISTORY[process[:pid]] << process[:cpu]
+      @CPU_HISTORY[process[:pid]] = @CPU_HISTORY[process[:pid]].last(@MAX_HISTORY)
+    end
+  end
+
+  def get_strace(pid, time)
+    %x(timeout #{time} strace -p #{pid} 2>&1) if system("which timeout > /dev/null")
+  end
+
+  def hunt_zombies
+    active_pids_in_passenger_status = passenger_pids
+    active_processes_in_processlist = process_status
+    zombies = active_processes_in_processlist.map{|x| x[:pid] } - active_pids_in_passenger_status
+
+    # kill processes with high CPU if user wants it
+    high_load = if @MAX_FAILS
+      store_current_cpu active_processes_in_processlist
+      active_pids_in_passenger_status.select do |pid|
+        @CPU_HISTORY[pid].count{|x| x > @MAX_CPU } >= @MAX_FAILS
+      end
+    else
+      []
+    end
+
+    (high_load + zombies).each do |pid|
+      kill_zombie pid
+    end
+  end
+
+  def passenger_pids
+    %x(passenger-status|grep PID).split("\n").map{|x| x.strip.match(/PID: \d*/).to_s.split[1]}.map(&:to_i)
+  end
+
+  def process_status
+    %x(ps -eo pid,pcpu,args|grep -v grep|grep '#{@PASSENGER_PROCESS_PATTERN}').split("\n").map do |line|
+       values = line.strip.split[0..1]
+       {:pid => values.first.to_i, :cpu => values.last.to_f}
+    end
+  end
+
+  def kill_zombie(pid)
+    puts "Killing passenger process #{pid}"
+    puts get_strace(pid, 5)
+    puts %x(kill #{pid})
+    sleep @GRACE_TIME
+    %x(kill -9 #{pid})
+  end
 end
--- spec/zombie_passenger_killer_spec.rb
+++ spec/zombie_passenger_killer_spec.rb
@@ -1,7 +1,58 @@
 require File.expand_path('spec/spec_helper')
 
 describe ZombiePassengerKiller do
+  let(:killer){
+    k = ZombiePassengerKiller.new(@options || {})
+    k.stub!(:passenger_pids).and_return([111])
+    k
+  }
+
   it "has a VERSION" do
     ZombiePassengerKiller::VERSION.should =~ /^\d+\.\d+\.\d+$/
+  end
+
+  it "does not kill anything by default" do
+    killer.should_not_receive(:kill_zombie)
+    killer.hunt_zombies
+  end
+
+  it "kill zombies" do
+    killer.stub!(:passenger_pids).and_return([123])
+    killer.stub!(:process_status).and_return([{:pid => 124, :cpu => 0}])
+    killer.should_receive(:kill_zombie).with(124)
+    killer.hunt_zombies
+  end
+
+  it "kills zombies with high cpu over max" do
+    @options = {:max => 1}
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+    killer.should_receive(:kill_zombie).with(111)
+    killer.hunt_zombies
+  end
+
+  it "does not kills zombies with high cpu under max" do
+    @options = {:max => 2}
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+    killer.should_not_receive(:kill_zombie).with(111)
+    killer.hunt_zombies
+  end
+
+  it "ignores high cpu levels in old history" do
+    @options = {:max => 2, :history => 2}
+    killer.should_not_receive(:kill_zombie).with(111)
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+    killer.hunt_zombies
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 0}])
+    killer.hunt_zombies
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+    killer.hunt_zombies
+  end
+
+  it "kills on high cpu levels in recent history" do
+    @options = {:max => 2, :history => 2}
+    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+    killer.hunt_zombies
+    killer.should_receive(:kill_zombie).with(111)
+    killer.hunt_zombies
   end
 end
