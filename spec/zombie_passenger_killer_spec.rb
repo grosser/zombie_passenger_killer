672d37d5e0707dc35f0d1608e6910ba1b96e2102
grosser <grosser.michael@gmail.com>
2012-01-04 21:49:58 +0100

    reformat specs

--- spec/zombie_passenger_killer_spec.rb
+++ spec/zombie_passenger_killer_spec.rb
@@ -11,56 +11,60 @@ describe ZombiePassengerKiller do
     ZombiePassengerKiller::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
 
-  it "does not kill anything by default" do
-    killer.should_not_receive(:kill_zombie)
-    killer.hunt_zombies
+  describe "#hunt_zombies" do
+    it "does not kill anything by default" do
+      killer.should_not_receive(:kill_zombie)
+      killer.hunt_zombies
+    end
+
+    it "kill zombies" do
+      killer.stub!(:passenger_pids).and_return([123])
+      killer.stub!(:process_status).and_return([{:pid => 124, :cpu => 0}])
+      killer.should_receive(:kill_zombie).with(124)
+      killer.hunt_zombies
+    end
+
+    it "kills zombies with high cpu over max" do
+      @options = {:max => 1}
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+      killer.should_receive(:kill_zombie).with(111)
+      killer.hunt_zombies
+    end
+
+    it "does not kills zombies with high cpu under max" do
+      @options = {:max => 2}
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+      killer.should_not_receive(:kill_zombie).with(111)
+      killer.hunt_zombies
+    end
+
+    it "ignores high cpu levels in old history" do
+      @options = {:max => 2, :history => 2}
+      killer.should_not_receive(:kill_zombie).with(111)
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+      killer.hunt_zombies
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 0}])
+      killer.hunt_zombies
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+      killer.hunt_zombies
+    end
+
+    it "kills on high cpu levels in recent history" do
+      @options = {:max => 2, :history => 2}
+      killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
+      killer.hunt_zombies
+      killer.should_receive(:kill_zombie).with(111)
+      killer.hunt_zombies
+    end
   end
 
-  it "kill zombies" do
-    killer.stub!(:passenger_pids).and_return([123])
-    killer.stub!(:process_status).and_return([{:pid => 124, :cpu => 0}])
-    killer.should_receive(:kill_zombie).with(124)
-    killer.hunt_zombies
-  end
+  describe "cli" do
+    it "prints its version" do
+      `./bin/zombie_passenger_killer -v`.should =~ /^\d+\.\d+\.\d+$/m
+    end
 
-  it "kills zombies with high cpu over max" do
-    @options = {:max => 1}
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
-    killer.should_receive(:kill_zombie).with(111)
-    killer.hunt_zombies
-  end
-
-  it "does not kills zombies with high cpu under max" do
-    @options = {:max => 2}
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
-    killer.should_not_receive(:kill_zombie).with(111)
-    killer.hunt_zombies
-  end
-
-  it "ignores high cpu levels in old history" do
-    @options = {:max => 2, :history => 2}
-    killer.should_not_receive(:kill_zombie).with(111)
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
-    killer.hunt_zombies
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 0}])
-    killer.hunt_zombies
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
-    killer.hunt_zombies
-  end
-
-  it "kills on high cpu levels in recent history" do
-    @options = {:max => 2, :history => 2}
-    killer.stub!(:process_status).and_return([{:pid => 111, :cpu => 100}])
-    killer.hunt_zombies
-    killer.should_receive(:kill_zombie).with(111)
-    killer.hunt_zombies
-  end
-
-  it "prints its version" do
-    `./bin/zombie_passenger_killer -v`.should =~ /^\d+\.\d+\.\d+$/m
-  end
-
-  it "prints help" do
-    `./bin/zombie_passenger_killer -h`.should include('Usage')
+    it "prints help" do
+      `./bin/zombie_passenger_killer -h`.should include('Usage')
+    end
   end
 end
